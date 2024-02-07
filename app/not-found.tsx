2151e6b9539758137504ed845375a502cf074239
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-02-07 23:03:00 +0100

    fix: center 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 function NotFoundPage() {
   return (
-    <div className="hero min-h-screen">
+    <div className="min-h-screen flex justify-center items-center flex-col">
       <div className="hero-content block text-center">
         <Image src="/not-found.png" className="mx-auto" alt="Page not found" width={700} height={700}/>
         <h1 className="text-4xl font-bold">Page not found</h1>

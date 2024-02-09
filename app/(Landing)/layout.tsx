ce47a29ece37f445239f1b60d3edb0f80fc62591
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-02-09 20:46:50 +0100

    feat: relayed UI layout to (Landing) and set root layout as an _app.tsx

---
+++ app/(Landing)/layout.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from "react";
+import "../../styles/globals.css";
+import VerAppBar from "@/components/VerAppBar";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+  return (
+    <>
+      <VerAppBar />
+      <div className="min-h-screen min-w-screen bg-gray-200 p-8">
+        <div className="container mx-auto bg-white p-4 rounded-lg">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
+import type { AppProps } from 'next/app'
+import {Inter} from "next/font/google";
+import type {Metadata} from "next";
 import React from "react";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../styles/globals.css";
-import VerAppBar from "@/components/VerAppBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,17 +11,11 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <VerAppBar />
-        <div className="min-h-screen min-w-screen bg-gray-200 p-8">
-          <div className="container mx-auto bg-white p-4 rounded-lg">
-            {children}
-          </div>
-        </div>
-      </body>
+    <body className={inter.className}>
+    {children}
+    </body>
     </html>
-  );
+  )
 }
